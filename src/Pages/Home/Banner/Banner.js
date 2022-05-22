4a5589c9a6623b7e77883cf0d3a945d54979c8f0
arburhan <buthan190629@outlook.com>
2022-05-22 18:31:24 +0600

    style add on banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -12,10 +12,11 @@ const Banner = () => {
                 width: '100%',
             }}
         >
-            <div className='flex items-center justify-center h-full' >
+            <div className='flex items-center justify-center h-full px-3' >
                 <div className='banner-text text-center'>
                     <h2 className='text-3xl md:text-5xl text-[#db011c]' >Tools That Your Need</h2>
-                    <p className='py-4' >Power Tools is a tools brand about providing tools for every work place. Distributing high end tools to help create your ideal vision for a project, home, or application. We are your partners, confidants and most importantly your guidance to help you complete projects that matter.</p>
+                    <p className='py-4' >Distributing high end tools to help create your ideal vision for a project, home, or application. We are your partners, confidants and most importantly your guidance to help you complete projects that matter.</p>
+                    <button className='details-btn rounded-full text-black px-12 py-4 hover:bg-[#db011c] hover:ease-linear duration-[.2s] hover:text-white' >View Details</button>
                 </div>
             </div>
 
