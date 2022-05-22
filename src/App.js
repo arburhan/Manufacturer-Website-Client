b99d05e60a3a21fcadb4775d3fe63c8d8c6ccde0
arburhan <buthan190629@outlook.com>
2022-05-22 10:58:06 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p className="text-3xl text-red-600">Power</p>
-      </header>
+    <div>
+      <Navbar></Navbar>
     </div>
   );
 }
