3935ea39033164ab7dca4d66bf315185eb554ed9
arburhan <buthan190629@outlook.com>
2022-05-22 22:40:39 +0600

    create tools components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Loading from '../Shared/Loading/Loading';
 import Banner from './Banner/Banner';
+import Tools from './Tools';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Tools></Tools>
 
         </div>
     );
---
+++ src/Pages/Home/Tools.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tools = () => {
+    return (
+        <div className='my-12'>
+            <h2 className="text-3xl text-center text-[#0f172a]">Tools</h2>
+        </div>
+    );
+};
+
+export default Tools;
