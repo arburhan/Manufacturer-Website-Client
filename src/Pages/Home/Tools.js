65ec335bb386c55ccb4ee6144906df4366d7a3e7
arburhan <buthan190629@outlook.com>
2022-05-23 00:09:31 +0600

    work on tools section

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -10,9 +10,9 @@ const Tools = () => {
     }, [])
     console.log(tools)
     return (
-        <div className='my-12'>
-            <h2 className="text-3xl text-center text-[#0f172a]">Tools</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3' >
+        <div className='my-12 container mx-auto'>
+            <h2 className="text-3xl text-center text-[#0f172a] font-bold"> <span className='underline underline-offset-4' >Tools</span> </h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' >
                 {
                     tools.slice(0, 6).map(tool => <ToolsCard tool={tool} ></ToolsCard>)
                 }
--- src/Pages/Home/ToolsCard.js
+++ src/Pages/Home/ToolsCard.js
@@ -3,17 +3,19 @@ import React from 'react';
 const ToolsCard = ({ tool }) => {
     const { name, image, description, minimumQuantity, availableQuantity, unitPrice } = tool;
     return (
-        <div className="hero bg-base-200">
+        <div className="card bg-base-100 shadow-md hover:shadow-2xl">
             <div className="hero-content text-center">
                 <div className="max-w-md">
                     <figure class="px-10 pt-5">
                         <img style={{ height: '225px', width: '338px' }} src={image} alt="power machine" class="rounded-xl" />
                     </figure>
                     <h1 className="text-2xl font-bold mt-3">{name}</h1>
-                    <p className="pt-2">{description.length > 130 ? description.slice(0, 130) + '...' : description} </p>
-                    <p className="py-1" >Available Quantity: <span className='text-secondary' >{availableQuantity}</span> Pcs</p>
+                    <p className="pt-2" title={description}>{description.length > 130 ? description.slice(0, 120) + '...' : description} </p>
+                    <p className="py-1 font-bold" >Available Quantity: <span className='text-secondary' >{availableQuantity}Pcs</span> </p>
+                    <p className=' font-bold'>Unit Price: <span className='text-secondary'> ${unitPrice}</span> </p>
+                    <p className='my-1 font-bold'>Minimum Order: <span className='text-secondary'> {minimumQuantity} pcs</span> </p>
 
-                    <button className="btn btn-primary">Purchase</button>
+                    <button className="btn btn-accent text-white my-2">Order Now</button>
                 </div>
             </div>
         </div>
