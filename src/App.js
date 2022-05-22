31de95b0affd545ac32e1806934ac6603d07066c
arburhan <buthan190629@outlook.com>
2022-05-22 11:35:20 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './Pages/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
