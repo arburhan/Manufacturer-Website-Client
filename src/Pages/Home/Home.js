c851d1f04640266be337205b2086354f7f95d81d
arburhan <buthan190629@outlook.com>
2022-05-22 12:11:09 +0600

    loading added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Loading from '../Shared/Loading/Loading';
 
 const Home = () => {
     return (
         <div>
+            <Loading></Loading>
 
         </div>
     );
