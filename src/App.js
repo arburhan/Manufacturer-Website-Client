9c8bd05aba0cd2f909613ca3303dd7d9ff76fb11
arburhan <buthan190629@outlook.com>
2022-05-23 22:49:13 +0600

    create dashboard

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp';
 import Footer from './Pages/Shared/Footer';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
         <Route path='/home' element={<Home></Home>} ></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}>
+          <Route index path='myorders' element={''} ></Route>
+          <Route path='myorders' element={''} ></Route>
+          <Route path='myorders' element={''} ></Route>
+        </Route>
       </Routes>
       <Footer></Footer>
     </div>
