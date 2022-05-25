d873813f233cbd36a4026ccc1a6f0c11e62c7c4e
arburhan <buthan190629@outlook.com>
2022-05-25 16:35:28 +0600

    try to fix space  problem

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -8,19 +8,18 @@ const Dashboard = () => {
             <div className="drawer-content">
                 {/* <!-- Page content here --> */}
                 <h2 className="text-3xl my-3">Dashboard</h2>
-
                 <Outlet></Outlet>
-
-
             </div>
-            <div className="drawer-side">
-                <label htmlFor="power-drawer" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard'>My Orders</Link></li>
-                    <li><Link to='/dashboard/addreview'>Add a Review</Link></li>
-                    <li><Link to='/dashboard/myprofile'>My Profile</Link></li>
-                </ul>
+            <div className='container mx-3'>
+                <div className="drawer-side">
+                    <label htmlFor="power-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                        {/* <!-- Sidebar content here --> */}
+                        <li><Link to='/dashboard'>My Orders</Link></li>
+                        <li><Link to='/dashboard/addreview'>Add a Review</Link></li>
+                        <li><Link to='/dashboard/myprofile'>My Profile</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
