484e18bed80d1708e1ff20c5565091c224f78916
arburhan <buthan190629@outlook.com>
2022-05-25 12:01:37 +0600

    add sweet alert

--- src/Pages/Dashboard/MyOrderTable.js
+++ src/Pages/Dashboard/MyOrderTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 const MyOrderTable = ({ order, index, refetch }) => {
     const { _id, productName, quantity, totalPrice } = order;
@@ -18,7 +19,24 @@ const MyOrderTable = ({ order, index, refetch }) => {
                 <th>{quantity}</th>
                 <th>$ {totalPrice}</th>
                 <th> <button className="btn btn-accent">Pay</button> </th>
-                <th> <button onClick={() => { handleDelete(_id) }} className='btn btn-error'>Cancel</button>  </th>
+                <th> <button onClick={() => {
+                    swal({
+                        title: "Are you sure?",
+                        text: "Once deleted, you will not be able to recover this order!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                        .then((willDelete) => {
+                            if (willDelete) {
+                                swal("Poof! Your imaginary file has been deleted!", {
+                                    icon: "success",
+                                });
+                            } else {
+                                swal("Your imaginary file is safe!");
+                            }
+                        })
+                }} className='btn btn-error'>Cancel</button>  </th>
             </tr>
 
         </>
