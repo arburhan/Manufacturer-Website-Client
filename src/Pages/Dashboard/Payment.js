330279edfa8cf302b08bbc750679a74f8969de9e
arburhan <buthan190629@outlook.com>
2022-05-26 11:24:44 +0600

    try to resolve error

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -23,16 +23,16 @@ const Payment = () => {
 
     return (
         <div>
-            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                <div class="card-body">
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
                     <p className="text-success font-bold">Hello, { }</p>
-                    <h2 class="card-title">Please Pay for <span className='text-primary'>{myorder.productName}</span> </h2>
+                    <h2 className="card-title">Please Pay for <span className='text-primary'>{myorder.productName}</span> </h2>
                     <p>Your Oradered Quantity: <span className='text-orange-700'>{myorder.quantity} pcs</span></p>
                     <p>Please pay: <span className='text-secondary'>${myorder.totalPrice}</span></p>
                 </div>
             </div>
-            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-                <div class="card-body">
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
                     <Elements stripe={stripePromise}>
                         <CheckoutForm myorder={myorder} />
                     </Elements>
