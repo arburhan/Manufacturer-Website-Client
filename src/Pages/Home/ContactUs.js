1e27cc39169eb7ee04840ae7d3e774f214f13ad1
arburhan <buthan190629@outlook.com>
2022-05-27 18:02:14 +0600

    style added

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -10,7 +10,7 @@ const ContactUs = () => {
                         <h2 className="text-2xl">Phone</h2>
                         <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
                             </svg>
                             <a href="tel:+8801737509339">+8801737509339</a>
                         </figure>
@@ -18,7 +18,7 @@ const ContactUs = () => {
                     <div className='my-5'>
                         <h2 className="text-2xl">Email</h2>
                         <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                             </svg>
@@ -28,8 +28,8 @@ const ContactUs = () => {
                     <div>
                         <h2 className="text-2xl">Address</h2>
                         <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                             </svg>
                             <address>Sector-3,Uposhohor Rajshahi </address>
                         </figure>
@@ -53,7 +53,7 @@ const ContactUs = () => {
                         <label className="label">
                             <span className="label-text">Message</span>
                         </label>
-                        <textarea class="textarea textarea-bordered h-24" placeholder="Your message is here"></textarea>
+                        <textarea className="textarea textarea-bordered h-24" placeholder="Your message is here"></textarea>
                     </div>
                     <div className="form-control mt-6">
                         <button className="btn btn-primary">Send</button>
